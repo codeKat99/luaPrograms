abbb7c82d711f669ebecf9c4e5ace02176167d08
codeKat99 <91572800+codeKat99@users.noreply.github.com>
2022-04-08 15:24:44 -0500

    Hello World program and json file
    It seems like the json file is required to make the program work in vscode - it was autogenerated once I got the extension working. Will be investigating the specifics later, so things could change

---
+++ hello.lua
@@ -0,0 +1,11 @@
+print("Hello World")
+
+temp = true
+if temp then
+    a=15  -- Notice that when we mouse over this, it is actually a global variable! Will be demonstrated below
+    local b = 66
+    temp=false
+end
+
+print("Hello world" , a)
+print("Oops, this won't work!", b) -- b is nil because we specifically declared it local.
