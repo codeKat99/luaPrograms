cb5b63ed721c0c3ed53f25b888647a465171513b
codeKat99 <91572800+codeKat99@users.noreply.github.com>
2022-04-30 20:02:08 -0500

    Testfile and new functions
    Added topX and bottomX functions.
    Added test file so you can run this anywhere, not ONLY on my laptop. lol

--- WordCount.lua
+++ WordCount.lua
@@ -1,6 +1,5 @@
-print("Here")
-
-file = io.open("C:/Users/thefi/Desktop/F Zone/2022-1 Spring Semester/IT 327/LuaProject/WordCount/Matthew.txt", "r")
+textfile = 'testfile.txt'
+file = io.open(textfile, "r")
 lines = file:lines()
 
 function split (inputstr, sep)
@@ -8,18 +7,19 @@ function split (inputstr, sep)
             sep = "%s" -- %s is whitespace in lua
     end
     local t={}
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do -- We do a little regex!
             table.insert(t, str)
     end
     return t
 end
-
+--[[
 tab = split("Split me up boss", " ")
 for i, v in pairs(tab) do
     print(i .. " " .. v)
 end
-wordCount = {}
 
+--]]
+wordCount = {}
 function manageWords (wordTable, word)
     if wordTable[word] ~= nil then
         wordTable[word] = wordTable[word] + 1
@@ -27,8 +27,40 @@ function manageWords (wordTable, word)
     end
 end
 
+function topX(wordTable, numWords)
+    local topXWords = {}
+    for i=1, numWords,1 do
+        local tempWordPair = {num=0, word=""}
+        for word, count in pairs(wordTable) do
+            if(topXWords[word]==nil and wordTable[word] > tempWordPair.num) then 
+                tempWordPair.num = wordTable[word]
+                tempWordPair.word=word
+            end
+        end
+        topXWords[tempWordPair.word]=tempWordPair.num
+        print(tempWordPair.word .. " appeared " .. tempWordPair.num .. " times!")
+    end
+    return topXWords
+end
+function bottomX(wordTable, numWords)
+    local topXWords = {}
+    for i=1, numWords,1 do
+        local tempWordPair = {num=99999999, word=""}
+        for word, count in pairs(wordTable) do
+            if(topXWords[word]==nil and wordTable[word] < tempWordPair.num) then 
+                tempWordPair.num = wordTable[word]
+                tempWordPair.word=word
+            end
+        end
+        topXWords[tempWordPair.word]=tempWordPair.num
+        print(tempWordPair.word .. " appeared " .. tempWordPair.num .. " times!")
+    end
+    return topXWords
+end
+    
 
-print("Contents of file:");
+
+print("Reading in file...");
 for line in lines do
     words = split(line, " ")
     for i, v in pairs(words) do
@@ -38,9 +70,14 @@ for line in lines do
             manageWords(wordCount,fixedWord)
     end
     end
-    print("\n")
+    --print("\n")
 end
-
+--[[]
 for key,value in pairs(wordCount) do
     print(key .. " appeared " .. value .. " times!")
 end
+--]]
+print("Calling for function to see what the top X=10 most frequent words were in the text: ".. textfile );
+topX(wordCount,10)
+print("Calling for function to see what 5 most INFREQUENT words were in the text: ".. textfile );
+bottomX(wordCount,10)
